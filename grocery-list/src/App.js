402d5a5296a7294ff31ba4450b402ea36f88edd6
Lamia Kabir <lkabir260@gmail.com>
2020-06-19 08:57:50 -0400

    added up to checkboxes

---
+++ grocery-list/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Take out the trash',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Dinner with wife',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'Meeting with boss',
+            completed: false
+        }
+    ]
+}
+
+markComplete = () => {
+  console.log('From App.js')
+}
+
+ render() { 
+  return (
+    <div className="App">
+      <Todos todos={this.state.todos} markComplete={this.markComplete}
+      />
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ grocery-list/src/components/Todoitem.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Todoitem extends Component {
+    getStyle = () => {
+        return{
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through' : 'none'
+        }
+    }
+
+    markComplete = (e) => {
+        console.log(this.props)
+
+    }
+ 
+    render() {
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete}/> {' '}
+                { this.props.todo.title }
+                </p>
+            </div>
+        )
+    }
+}
+// PropTypes
+Todoitem.propTypes = {
+    todo: PropTypes.object.isRequired
+
+}
+
+
+export default Todoitem
